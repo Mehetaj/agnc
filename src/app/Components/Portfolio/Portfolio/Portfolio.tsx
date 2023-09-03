906c186821aab421abaf1019ab99fdf576209bf4
shakilkhanSAU <shakilkhan.sau@gmail.com>
2023-09-03 15:27:25 +0600

    portfolio items added

--- src/app/Components/Portfolio/Portfolio/Portfolio.tsx
+++ src/app/Components/Portfolio/Portfolio/Portfolio.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import './Tab.css'
+import './Portfolio.css'
+import cover1 from '../../../../asset/portfolio/cover1.png'
+import cover2 from '../../../../asset/portfolio/cover2.png'
+import cover3 from '../../../../asset/portfolio/cover3.png'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -8,7 +12,7 @@ const Portfolio = (props: Props) => {
         <>
             {/* tab section of portfolio */}
             <div className=' w-3/4	'>
-                <div className="flex justify-around item-center flex-1 PortfolioContaner">
+                <div className="flex justify-around item-center flex-1  tabContaner">
                     <button className='px-12 font-bold'>View All</button>
                     <button className='px-12'>Graphics</button>
                     <button className='px-12'>UI/UX</button>
@@ -18,8 +22,15 @@ const Portfolio = (props: Props) => {
             </div>
 
             {/* portfolio section */}
-            <div className='flex flex-wrap justify-center items-center'>
-
+            <div className='grid grid-cols-6 gap-4'>
+                <div className='col-span-2 p-5'>
+                    {/* Banner image */}
+                    <Image className='' src={cover1} alt='' />
+                    <div>
+                        <p className='font-light'>Landing Page</p>
+                        <p className='font-bold text-2xl'>Dentistry Website</p>
+                    </div>
+                </div>
 
 
             </div>
