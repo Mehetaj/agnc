935c01082cd9c08c98cb13b9f95f4e9b6a4e8039
shakilkhanSAU <shakilkhan.sau@gmail.com>
2023-09-03 15:49:58 +0600

    all okay in portfolio section

--- src/app/Components/Portfolio/Portfolio/Portfolio.tsx
+++ src/app/Components/Portfolio/Portfolio/Portfolio.tsx
@@ -24,101 +24,108 @@ const Portfolio = (props: Props) => {
 
             {/* portfolio section */}
             <div className='grid grid-cols-6 gap-4 w-full'>
-                {/* portfolio item first row */}
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                {/* portfolio item --------------- first row */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* ------------ Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* ----------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>Landing Page</p>
                         <p className='font-bold text-2xl'>Dentistry Website</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover2} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/*   ------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Vegmart app Case Study</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* --------------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Music Event app Case Study</p>
                     </div>
                 </div>
 
-                {/* portfolio item second row */}
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+
+
+                {/* portfolio item ------------ second row */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* ------------ Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* ----------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>Landing Page</p>
                         <p className='font-bold text-2xl'>Dentistry Website</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover2} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/*   ------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Vegmart app Case Study</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* --------------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Music Event app Case Study</p>
                     </div>
                 </div>
 
-                {/* portfolio item third row */}
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+
+                {/* portfolio item ---------- third row */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* ------------ Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* ----------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>Landing Page</p>
                         <p className='font-bold text-2xl'>Dentistry Website</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover2} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/*   ------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Vegmart app Case Study</p>
                     </div>
                 </div>
-                <div className='col-span-2 p-5 portfolio-item'>
-                    {/* Banner image */}
+                <div className='col-span-2 p-4 portfolio-item'>
+                    {/* -------------- Banner image */}
                     <Image className='' src={cover1} alt='' />
 
-                    {/* portfolio description */}
-                    <div className='py-4'>
+                    {/* --------------- portfolio description */}
+                    <div className='py-3'>
                         <p className='font-light'>App Development</p>
                         <p className='font-bold text-2xl'>Music Event app Case Study</p>
                     </div>
                 </div>
+
+
+
+                {/* portfolio section ----------- end  */}
             </div>
 
 
