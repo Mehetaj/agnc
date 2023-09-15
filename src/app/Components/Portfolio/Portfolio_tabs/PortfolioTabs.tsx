05330a9e0572160748ea14bd075a0229cf3a2c88
MH-Fahim11 <fmahmudulhasan@gmail.com>
2023-09-15 12:20:02 +0600

    Tabs now working with dinamicly

--- src/app/Components/Portfolio/Portfolio_tabs/PortfolioTabs.tsx
+++ src/app/Components/Portfolio/Portfolio_tabs/PortfolioTabs.tsx
@@ -1,94 +1,26 @@
 "use client"
-import { useState } from 'react'
 import { Tab } from '@headlessui/react'
-
+import { portfolio_data } from "@/app/FakeData/portfolio";
+import Single_portfolio from '../../Home/Portfolio/singleportfolio';
 function classNames(...classes : any | string[] | string | object) {
   return classes.filter(Boolean).join(' ')
 }
 
+
 export default function Portfolio_tabs() {
-  let [categories] = useState({
-    View_All: [
-      {
-        id: 1,
-        title: 'Does drinking coffee make you smarter?',
-        date: '5h ago',
-        commentCount: 5,
-        shareCount: 2,
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: '2h ago',
-        commentCount: 3,
-        shareCount: 2,
-      },
-    ],
-    Graphics: [
-      {
-        id: 1,
-        title: 'Is tech making coffee better or worse?',
-        date: 'Jan 7',
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: 'The most innovative things happening in coffee',
-        date: 'Mar 19',
-        commentCount: 24,
-        shareCount: 12,
-      },
-    ],
-    UX_UI: [
-      {
-        id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        date: '2d ago',
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: '4d ago',
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-    Wordpress: [
-      {
-        id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        date: '2d ago',
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: '4d ago',
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-    Web_Design: [
-      {
-        id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        date: '2d ago',
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: '4d ago',
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-  })
+  const categorizedData: any = {
+    "View all": [...portfolio_data], // Create a "View all" category with a copy of all data
+};
+
+  portfolio_data.forEach(item => {
+      const { category, ...rest } = item;
+      if (!categorizedData[category]) {
+          categorizedData[category] = [];
+      }
+      categorizedData[category].push(rest);
+  });
+
+ let categories = categorizedData;
 
   return (
     <div className="w-full  mx-auto px-2 py-16 sm:px-0">
@@ -112,7 +44,7 @@ export default function Portfolio_tabs() {
           ))}
         </Tab.List>
         <Tab.Panels className="mt-2">
-          {Object.values(categories).map((posts, idx) => (
+          {Object.values(categories).map((category:any, idx) => (
             <Tab.Panel
               key={idx}
               className={classNames(
@@ -120,34 +52,11 @@ export default function Portfolio_tabs() {
                 ''
               )}
             >
-              <ul>
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
-                  >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        'absolute inset-0 rounded-md',
-                        ''
-                      )}
-                    />
-                  </li>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16">
+                {category.map((data:any) => (
+                  <Single_portfolio key={data.id} data={data} id={0} img={''} category={''} name={''}></Single_portfolio>
                 ))}
-              </ul>
+              </div>
             </Tab.Panel>
           ))}
         </Tab.Panels>
--- src/app/FakeData/portfolio.ts
+++ src/app/FakeData/portfolio.ts
@@ -1,21 +1,36 @@
 import img1 from '../../asset/portfolio/img1.png'
 import img2 from '../../asset/portfolio/img2.png'
 import img3 from '../../asset/portfolio/img3.png'
+interface Project {
+    id: number;
+    img: string;
+    name: string;
+    category: string;
+}
 
-export const portfolio_data = [
+export const portfolio_data: Project[] = [
     {
+        id:1,
         img: "https://i.ibb.co/kK4R233/img1.png",
         name: "Dentistry website",
         category: "Landing page"
     },
     {
+        id:2,
         img: "https://i.ibb.co/L6LdYCt/img2.png",
         name: "Vegmart app case study",
         category: "App Design"
     },
     {
+        id:3,
         img: "https://i.ibb.co/X42g6Vy/img3.png",
         name: "Music Event app case study",
         category: "App Design"
     },
+]
+
+export const categories =[
+    "View all",
+    "Landing page",
+    "App Design"
 ]
