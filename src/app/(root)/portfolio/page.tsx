9cfc0bd2409e5309e8cf7b732ec6f2eb6fc5a1a8
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-09-16 10:25:52 +0600

    bugs fixed

--- src/app/(root)/portfolio/page.tsx
+++ src/app/(root)/portfolio/page.tsx
@@ -1,24 +1,14 @@
 // tsx page component
-
 import PortfolioBanner from "@/app/Components/Portfolio/PortfolioBanner/PortfolioBanner";
 import React from "react";
 import Contact from "@/app/Components/Home/Contact/Contact";
 import Component from "@/app/Shared/Component/Component";
-import Portfolio from "@/app/Components/Home/Portfolio/Portfolio";
 
 const page = () => {
   return (
     <div>
       <Component>
         <PortfolioBanner />
-<<<<<<< HEAD
-        <div className="">
-=======
-        <Portfolio_tabs />
-        {/* <div className="">
->>>>>>> 947ac79e882378857c4f401c8654a899cf6459b4
-          <Portfolio />
-        </div> */}
       </Component>
       <Contact />
     </div>
