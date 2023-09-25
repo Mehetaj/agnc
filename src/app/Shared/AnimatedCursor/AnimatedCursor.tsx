9ff056b2c3dde829c4f9bca09c79be505d951ed3
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-09-25 17:17:28 +0600

    animated cursor problem fixed

--- src/app/Shared/AnimatedCursor/AnimatedCursor.tsx
+++ src/app/Shared/AnimatedCursor/AnimatedCursor.tsx
@@ -25,14 +25,6 @@ export default function Cursor() {
         '.link',
         {
           target: '.custom',
-          options:  {
-            innerSize: 12,
-            outerSize: 12,
-            color: '255, 255, 255',
-            outerAlpha: 0.3,
-            innerScale: 0.7,
-            outerScale: 5
-          }
         }
       ]}
     />
