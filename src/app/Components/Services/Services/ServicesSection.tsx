92d1970740863f20e071d30745ca33ffb81ab2c3
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-09-29 18:57:05 +0600

    key prop added to serviceSection

--- src/app/Components/Services/Services/ServicesSection.tsx
+++ src/app/Components/Services/Services/ServicesSection.tsx
@@ -12,7 +12,7 @@ const ServicesSection = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                     {
                         data?.map((d, i) => (
-                            <div className="bg-[#EBF2FF] dark:bg-[#0A1A33] dark:text-white mt-3 p-8 mb-10 w-[379px] h-[348px] rounded-lg">
+                            <div key={i} className="bg-[#EBF2FF] dark:bg-[#0A1A33] dark:text-white mt-3 p-8 mb-10 w-[379px] h-[348px] rounded-lg">
                                 <img src={d?.logo} alt="" />
                                 <h2 className="text-2xl font-bold my-6">{d?.category}</h2>
                                 <p>{d?.description}</p>
