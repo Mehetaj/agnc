72acef4bacc66f4ccb2ad0f9c80bc0e08dfa2a9f
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-10-11 16:34:53 +0600

    faileure

--- src/app/dashboard/user/layout.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: string | JSX.Element | JSX.Element[];
-};
-
-const UserLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
-};
-
-export default UserLayout;
