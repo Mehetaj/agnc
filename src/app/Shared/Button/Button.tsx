b5224d5d84a7c45e4555f012da29f5e0f138ed50
Mehetaj Khandaker <mehetajkhandaker@gmail.com>
2023-10-11 16:37:25 +0600

    janina ki hoilo

--- src/app/Shared/Button/Button.tsx
+++ src/app/Shared/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({ children }: Props) => {
     return (
         <button className="w-full md:w-fit bg-gradient-to-r from-[#5271FF] via-30% to-[#5E17EB] to-90%  flex items-center justify-center gap-2 text-lg py-3 px-5 rounded-xl  transition-transform text-white">
             {children}
-            <img alt='' src={arrow} />
+            <Image alt='' src={arrow} />
         </button>
     );
 };
